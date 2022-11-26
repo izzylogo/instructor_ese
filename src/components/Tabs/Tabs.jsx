529b3570d9939d99adeb98630d75c05fe0d3a02d
Israel Omitiran <omitiranisrael@gmail.com>
2022-11-26 20:05:04 +0100

    feat:making some little changes

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -39,16 +39,16 @@ const Tabs = () => {
         head={"TOP TRACKS"}
             tit1={"Title"}
             tit2={"Royalty"}
-            name1={"Star Girl"}
-            money1={"$17,359.26"}
-            name2={"Ije Cave"}
-            money2={"$9,368.48"}
-            name3={"Tunestar"}
-            money3={"$2,193.50"}
-            name4={"Steveland"}
-            money4={"$701.81"}
-            name5={"Ije Cave, Flavour"}
-            money5={"$103.71"}
+            name1={"Nwa Aba"}
+            money1={"$8,359.26"}
+            name2={"My Place"}
+            money2={"$4,368.48"}
+            name3={"One Thousand"}
+            money3={"$4,193.50"}
+            name4={"Remember"}
+            money4={"$4,701.81"}
+            name5={"My Place"}
+            money5={"$1,115"}
         />
         <Tab
         head={"TOP ARTISTS"}
